582dbfb8c1c54bd2b9cd427a23126d6778242b0c
brunoferreiraff <77634161+brunoferreiraff@users.noreply.github.com>
2026-06-30 17:44:38 -0300

    Final commit

--- CadastroFilme/src/main/java/com/filme/FilmeDAO.java
+++
@@ -1,8 +0,0 @@
-package com.filme;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FilmeDAO extends JpaRepository<Filme, Long> {
-}
